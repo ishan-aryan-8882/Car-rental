0d2bc6eeaa2c13e4c6d35adafc470b824910e7d5
Ishan Aryan <133885758+ishan-aryan-8882@users.noreply.github.com>
2023-09-15 13:49:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,11 +24,39 @@ const addClassWhenNear = (selector, className, distance) => {
 };
 
 addClassWhenNear(".ms", "showup", 5);
+
 addClassWhenNear(".sr", "showup", 5);
+
 addClassWhenNear(".um", "showup", 5);
+
 addClassWhenNear(".ow", "showup", 5);
 
 let logo = document.querySelector(".logo");
 logo.style.transform = "translateY(0%)";
 
 
+let faqwrap = document.querySelector(".faq-wrap");
+let faq = document.querySelector("#faq");
+window.addEventListener('scroll',()=>{
+  let rect = faqwrap.getBoundingClientRect();
+  if(rect.top < window.innerHeight){
+    faq.classList.add("da-an");
+  }
+  else{
+    faq.classList.remove("da-an");
+  }
+})
+
+
+
+let daWrap = document.querySelector('.da-wrapper');
+let da = document.querySelector('.da');
+window.addEventListener('scroll', () => {
+  let rect = daWrap.getBoundingClientRect();
+  if (rect.top < window.innerHeight) {
+    da.classList.add("da-an");
+  }
+  else{
+    da.classList.remove("da-an");
+  }
+});
